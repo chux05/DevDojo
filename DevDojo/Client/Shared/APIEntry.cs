62d29576e0324e258a09d3d1a07f85d0e100f957
Tshegofatso <Tshegofatso@LAP-JHB-005>
2022-02-07 13:59:57 +0200

    API page

---
+++ DevDojo/Client/Shared/APIEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevDojo.Client.Shared
+{
+    public class APIEntry
+    {
+        public string API { get; set; }
+        public string Description { get; set; }
+        public string  Auth { get; set; }
+        public bool HTTPS { get; set; }
+
+        public string Cors { get; set; }
+        public string Link { get; set; }
+        public string Category { get; set; }
+    }
+}
---
+++ DevDojo/Client/Shared/APIFull.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevDojo.Client.Shared
+{
+    public class APIFull
+    {
+        public List<APIEntry> Entries { get; set; }
+
+        public int Count { get; set; }
+    }
+}
